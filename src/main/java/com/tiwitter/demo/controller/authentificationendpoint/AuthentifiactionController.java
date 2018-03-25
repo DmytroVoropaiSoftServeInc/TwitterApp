6bee9858cc4ee7fdfe822e54152c53d702796f0c
dvorop <dimon1165@gmail.com>
2018-03-25 23:44:50 +0300

    continue spring security user authentification

---
+++ src/main/java/com/tiwitter/demo/controller/authentificationendpoint/AuthentifiactionController.java
@@ -0,0 +1,7 @@
+package com.tiwitter.demo.controller.authentificationendpoint;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthentifiactionController {
+}
